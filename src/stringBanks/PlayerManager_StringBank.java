fe1bba5dd7cdd5822c19daa86e09bfc7d42226c5
lasse <lassejensen213@gmail.com>
2016-10-31 15:59:51 +0100

    Jimmy update

--- src/stringBanks/PlayerManager_StringBank.java
+++ src/stringBanks/PlayerManager_StringBank.java
@@ -3,17 +3,19 @@ package stringBanks;
 public class PlayerManager_StringBank {
 
 	
+	private static String genericPlayerName = "Player";
+	
 	//Choose character
 	private static String character = "Choose between the following characters: ";
+
 	//Choose name
-	private static String name = "Pleace choose your name";
+	private static String name = "Please choose your name";
 	
 	//Name taken
 	private static String nametaken = "Name has already been taken. Please enter a new one";
 	
 	//Invalid character choice
-	private static String invalidchar = "Invalid character choice. \n\t Please enter your choice of character.";
-	
+	private static String invalidchar = "Invalid character choice. \n\t Pleace enter your choice of character.";
 	
 	
 	
@@ -36,5 +38,10 @@ public class PlayerManager_StringBank {
 	
 	public static String getInvalidChar() {
 		return invalidchar;
+	}
+	
+	public static String getGenericPlayer()
+	{
+		return genericPlayerName;
 	}
 }
